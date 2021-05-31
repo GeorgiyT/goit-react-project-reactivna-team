6106e89a10f8c7ed48cf1f52444c57ad1e49fb65
Georgiy Tonkiy <georgiy.tonkiy@gmail.com>
2021-05-31 15:48:49 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import UserHeader from "./UserHeader/UserHeader";
-<<<<<<< Updated upstream
 import LoginHeader from "./LoginHeader/LoginHeader";
 import { connect } from "react-redux";
 import { isAuthenticatedSelector } from "../../redux/auth/authSelectors";
@@ -8,17 +7,7 @@ import { isAuthenticatedSelector } from "../../redux/auth/authSelectors";
 const Header = ({ isAuth }) => {
   return <>{isAuth ? <UserHeader /> : <LoginHeader />}</>;
 };
-const mapStateToProps = (state) => ({
-  isAuth: isAuthenticatedSelector(state),
+const mapStateToProps = state => ({
+  isAuth: isAuthenticatedSelector(state)
 });
 export default connect(mapStateToProps)(Header);
-=======
-import isAuth from "../../auth/"
-export default function Header() {
-  return (
-    <header>
-      {true ? <LoginHeader /> : <UserHeader />}
-    </header>
-  );
-}
->>>>>>> Stashed changes
